98bf1517eb0edcfb373059e2c8d6b85dcefa5bc9
Adrian Eguez <eadepto@hotmail.com>
2018-10-12 17:08:22 -0500

    unde

--- 01-javascript/01-variables.js
+++ 01-javascript/01-variables.js
@@ -5,14 +5,14 @@ var edadString = "10";
 var sueldo = 1.234;
 var casado = false;
 var hijos = null;
-var dato;
+var dato = undefined;
 
 console.log("edad", typeof edad);
 console.log("edadString", typeof edadString);
 console.log("sueldo", typeof sueldo);
 console.log("casado", typeof casado); // boolean
 console.log("hijos", typeof hijos); // object
-console.log("dato", typeof dato); // object
+console.log("dato", typeof dato); // undefined
 
 
 
