c307128d7c88a2fbaf4825217b4c3b64adf2190a
Adrian Eguez <eadepto@hotmail.com>
2018-10-12 17:05:21 -0500

    variables

---
+++ 01-javascript/01-variables.js
@@ -0,0 +1,17 @@
+// Int edad = 10; TIPADO
+// NO TIPADO
+var edad = 10;
+var edadString = "10";
+var sueldo = 1.234;
+var casado = false;
+var hijos = null;
+
+
+console.log("edad", typeof edad);
+console.log("edadString", typeof edadString);
+console.log("sueldo", typeof sueldo);
+console.log("casado", typeof casado); // boolean
+console.log("hijos", typeof hijos); // object
+
+
+
