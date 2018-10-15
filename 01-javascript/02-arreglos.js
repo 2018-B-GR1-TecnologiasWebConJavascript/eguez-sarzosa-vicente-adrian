f154d50101c78197f4bc849118dede7408fed75a
Adrian Eguez <eadepto@hotmail.com>
2018-10-15 17:02:50 -0500

    terminar arreglos

--- 01-javascript/02-arreglos.js
+++ 01-javascript/02-arreglos.js
@@ -42,11 +42,34 @@ var arregloUno = arregloNumeros.slice(0, 3);
 
 var arregloDos = arregloNumeros.slice(3, 7);
 
-arregloDos.push(7);
-var indiceSiete = arregloDos.indexOf(7); // Indice de la primera ocurrencia
-console.log(arregloUno);
-console.log(arregloDos);
-console.log(indiceSiete);
+var arregloUnoDos = [1, 2];
+var arregloSeis = [6];
+
+// Destructuracion de arreglos
+
+console.log(...arregloUnoDos); //
+console.log(1, 2); //
+var arregloTotal = [...arregloUnoDos,
+    ...arregloUno,
+    ...arregloSeis,
+    ...arregloDos];
+
+console.log(arregloTotal);
+
+var arregloSiguientesNumeros = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+arregloTotal.splice(arregloTotal.length, 0, ...arregloSiguientesNumeros);
+
+console.log(arregloTotal);
+
+// [1,2,3,4,5,6,7,8,9,10]
+
+
+// arregloDos.push(7);
+// var indiceSiete = arregloDos.indexOf(7); // Indice de la primera ocurrencia
+// console.log(arregloUno);
+// console.log(arregloDos);
+// console.log(indiceSiete);
 
 // [0,10] Cerrado Esta incluido el 10
 // [0,10[ Abierto Esta excluido el 10
