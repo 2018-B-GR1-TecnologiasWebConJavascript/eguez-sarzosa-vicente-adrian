de5d56a7e7f1fc8e83a79b3bfaea7f34b695a098
Adrian Eguez <eadepto@hotmail.com>
2019-01-11 18:05:50 -0500

    formularios

--- 03-Angular/videos/src/app/rutas/ruta-crear-raza/ruta-crear-raza.component.ts
+++ 03-Angular/videos/src/app/rutas/ruta-crear-raza/ruta-crear-raza.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {RazaRestService} from '../../servicios/rest/raza-rest.service';
+import {Raza} from '../../interfaces/raza';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-ruta-crear-raza',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutaCrearRazaComponent implements OnInit {
 
-  constructor() { }
+  raza: RazaEjemplo = {
+    nombre: '123123123123',
+    apellido: ''
+  };
+
+  constructor(private readonly _razaRestService: RazaRestService) {
+  }
 
   ngOnInit() {
   }
 
+  crearRaza(formulario: NgForm) {
+
+    console.log('Formulario: ', formulario);
+
+
+    const crearRaza$ = this._razaRestService
+      .create(<string> this.raza.nombre);
+
+    crearRaza$
+      .subscribe(
+        (raza: Raza) => {
+          console.log('Raza');
+          alert(`Raza creada: ${raza.nombre}`);
+        },
+        (error) => {
+          console.error('Error: ', error);
+        }
+      );
+
+  }
+
+
+  mostrarEnConsola(objeto) {
+    console.log(objeto);
+    console.log(objeto.value);
+  }
+
 }
+
+
+interface RazaEjemplo {
+  nombre: string | number;
+  apellido: string;
+}
+
+
+
+
+
+
+
+
+
