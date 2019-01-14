73d37272275021157e44619ec76d959233bc8b42
Adrian Eguez <eadepto@hotmail.com>
2019-01-14 16:44:29 -0500

    ruta

--- 03-Angular/videos/src/app/rutas/ruta-actualizar-raza/ruta-actualizar-raza.component.ts
+++ 03-Angular/videos/src/app/rutas/ruta-actualizar-raza/ruta-actualizar-raza.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {RazaRestService} from '../../servicios/rest/raza-rest.service';
 
 @Component({
   selector: 'app-ruta-actualizar-raza',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RutaActualizarRazaComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly _activatedRoute: ActivatedRoute,
+    private readonly _razaRestService:RazaRestService
+  ) { }
 
+  // Cuando empieza el componente
   ngOnInit() {
+    const rutaActiva$ = this._activatedRoute.params;
+
+    rutaActiva$
+      .subscribe(
+        (parametros)=>{
+          
+        }
+      )
+
   }
 
 }
+
+
+
+
+
