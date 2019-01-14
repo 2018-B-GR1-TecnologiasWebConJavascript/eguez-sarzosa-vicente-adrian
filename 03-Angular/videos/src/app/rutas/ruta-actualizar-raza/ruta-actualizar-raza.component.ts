8675412b9cca92e92e9a099e343d82d23c59615a
Adrian Eguez <eadepto@hotmail.com>
2019-01-14 17:03:10 -0500

    formulario

--- 03-Angular/videos/src/app/rutas/ruta-actualizar-raza/ruta-actualizar-raza.component.ts
+++ 03-Angular/videos/src/app/rutas/ruta-actualizar-raza/ruta-actualizar-raza.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {RazaRestService} from '../../servicios/rest/raza-rest.service';
+import {Raza} from '../../interfaces/raza';
 
 @Component({
   selector: 'app-ruta-actualizar-raza',
@@ -9,10 +10,13 @@ import {RazaRestService} from '../../servicios/rest/raza-rest.service';
 })
 export class RutaActualizarRazaComponent implements OnInit {
 
+  razaAActualizar: Raza;
+
   constructor(
     private readonly _activatedRoute: ActivatedRoute,
-    private readonly _razaRestService:RazaRestService
-  ) { }
+    private readonly _razaRestService: RazaRestService
+  ) {
+  }
 
   // Cuando empieza el componente
   ngOnInit() {
@@ -20,16 +24,45 @@ export class RutaActualizarRazaComponent implements OnInit {
 
     rutaActiva$
       .subscribe(
-        (parametros)=>{
+        (parametros: ParametrosRutaActualizarRaza) => {
+          const raza$ = this._razaRestService
+            .findOneById(parametros.idRaza);
+
+          raza$
+            .subscribe(
+              (raza: Raza) => {
+                this.razaAActualizar = raza;
+              },
+              (error) => {
+                console.error('Error: ', error);
+              }
+            );
 
         }
-      )
+      );
 
+  }
+
+  buscarRaza(idRaza) {
+    const raza$ = this._razaRestService
+      .findOneById(idRaza);
+
+    raza$
+      .subscribe(
+        (raza: Raza) => {
+          console.log(raza);
+        },
+        (error) => {
+          console.error('Error: ', error);
+        }
+      );
   }
 
 }
 
-
+interface ParametrosRutaActualizarRaza {
+  idRaza: string;
+}
 
 
 
