143373f7a7ddd67bb31eaad42c2d73941a9ec8a2
Adrian Eguez <eadepto@hotmail.com>
2019-01-21 17:58:14 -0500

    CREAR RAZAS CON USERNAME PASSWORD

--- 03-Angular/videos/src/app/rutas/ruta-crear-raza/ruta-crear-raza.component.ts
+++ 03-Angular/videos/src/app/rutas/ruta-crear-raza/ruta-crear-raza.component.ts
@@ -25,7 +25,7 @@ export class RutaCrearRazaComponent implements OnInit {
 
     // if (this.nombreContieneA(razaObjeto.nombre.toString())) {
     const crearRaza$ = this._razaRestService
-      .create(razaObjeto.nombre);
+      .create(razaObjeto.nombre, razaObjeto.username,razaObjeto.password);
 
     crearRaza$
       .subscribe(
